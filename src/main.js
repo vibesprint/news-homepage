c9091962c1ff33c1fcd69953ad5b3d7550c2d5a4
admin <admin@local>
2026-07-08 18:37:25 +0530

    accessibility pass

--- src/main.js
+++ src/main.js
@@ -4,9 +4,26 @@ const menu_icon = nav.querySelector('#hamburger-menu')
 
 menu_icon.addEventListener('click', () => {
     nav_menu.classList.add('open')
+    menu_icon.setAttribute('aria-expanded', "true");
+    nav_menu.style.visibility = "visible";
+
+    nav_menu.querySelector('a')?.focus()
 })
+
+function closeMenu() {
+    nav_menu.classList.remove('open')
+    menu_icon.setAttribute('aria-expanded', 'false')
+    nav_menu.style.visibility = "hidden";
+}
 
 const close_menu_btn = nav_menu.querySelector('.heading')
 close_menu_btn.addEventListener('click', () => {
-    nav_menu.classList.remove('open')
+    closeMenu()
+})
+
+nav_menu.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        closeMenu()
+        menu_icon.focus();
+    }
 })
